eb573d7fdcae2feef72fb51805ee2e64accd1f07
Carl Carenvall <Carl.Carenvall@gmail.com>
2012-12-30 21:19:23 +0100

    draw stuff from pasture

--- pasture.erl
+++ pasture.erl
@@ -6,29 +6,40 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([init/0]).
+-export([init/0, start/0]).
 
 % Returns the table identifier of the ets table
 -spec init() -> integer().
 init() -> 
-    spawn_link(pasture,run,[ets:new('grid', [bag])]).
+    spawn_link(pasture,start,[]).
 
 
 init_test_() ->
     Grid = init(),
     [?_assert(ets:lookup(Grid, {0,0}) =:= [])].
 
-    
+
+start() ->
+    Grid = ets:new('grid', [bag]),
+    ets:insert(Grid, [{{1,1},purple}, {{3,3},green}]),
+    run(Grid).
     
 run(Grid) ->
+    % io:put_chars("pasture up and running\n"),
     receive
         after 100 ->
             %% update
-            pasture:draw(Grid),
-            pasture:run(Grid)
+            drawAll(Grid, ets:first(Grid)),
+            run(Grid)
         end.
 
-        
-draw(Grid) ->
-    frame ! {change_cell, 5, 5, purple}.
+drawAll(Grid, '$end_of_table') -> ok;
+drawAll(Grid, Curr) -> 
+    draw(ets:lookup(Grid, Curr)),
+    drawAll(Grid, ets:next(Grid, Curr)).
+    
+
+draw([{{X, Y},Type} | Rest]) ->
+    % io:put_chars("Attempting to draw once\n"),
+    frame ! {change_cell, X, Y, Type}.
     
