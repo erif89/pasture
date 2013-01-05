798b6b93f003119917e3a3153914360913ced580
Emil Wall <erif89@gmail.com>
2013-01-05 05:38:07 +0100

    Add comments

--- rabbit.erl
+++ rabbit.erl
@@ -30,14 +30,18 @@ navigate(Grid, X, Y) ->
     % Receive answers
     Receive = fun (_) -> receive {pong, Type, Pos} -> {Type, Pos} end end,
     Answers = lists:map(Receive, lists:seq(1, NumMsgs)),
+    % Filter out the different entities
     IsType = fun (Atom) -> fun ({Type, _}) -> Type =:= Atom end end,
     Rabbits = lists:filter(IsType(rabbit), Answers),
     Foxes = lists:filter(IsType(fox), Answers),
     Fences = lists:filter(IsType(fence), Answers),
     Grass = lists:filter(IsType(grass), Answers),
+    % Construct prioritized list of positions and take the first valid one
+    % TODO shuffle the Moves list
     Candidates = lists:map(fun ({_, Pos}) -> Pos end, Grass) ++ Moves,
     decide(Rabbits ++ Foxes ++ Fences, {X, Y}, Candidates).
 
+% Sends ping msgs to all Pids in the list and returns its length
 sendQueries(_Grid, [], Acc) -> Acc;
 sendQueries(Grid, [Pos|Rest], Acc) ->
     Stuff = ets:lookup(Grid, Pos),
@@ -45,6 +49,7 @@ sendQueries(Grid, [Pos|Rest], Acc) ->
     NumMsgs = lists:mapfoldl(Send, 0, Stuff),
     sendQueries(Grid, Rest, Acc + NumMsgs).
 
+% Returns the first position in Candidates (3rd arg) that does not collide
 decide(_Obstacles, OldPos, []) ->
     OldPos;
 decide(Obstacles, OldPos, [NewPos|Rest]) ->
